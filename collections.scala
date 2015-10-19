24a01877f4404a7cebcef797e7867cc389a4b69a
Harold Kasperink <harold.kasperink@gmail.com>
2015-10-19 16:53:20 +0200

    collections

---
+++ collections.scala
@@ -0,0 +1,14 @@
+// Define List of integers. 
+val x = List(1,2,3,4) 
+
+// Define a set. 
+var x = Set(1,3,5,7) 
+
+// Define a map. 
+val x = Map("one" -> 1, "two" -> 2, "three" -> 3) 
+
+// Create a tuple of two elements. 
+val x = (10, "Scala") 
+
+// Define an option 
+val x:Option[Int] = Some(5) 
